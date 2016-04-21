ad96693a51c7bc6d05982efdfe182d8a517fd1f9
Christopher Wiley <wiley@google.com>
2016-04-21 10:48:02 -0700

    libhardware_legacy should export headers from libmedia
    This library includes headers from libmedia in its public headers.
    It should export libmedia's include path as part of its own.
    
    Bug: 27804373
    Change-Id: I85ff45d0c02009277430e3cf66c170d824ed28e3

--- Android.mk
+++ Android.mk
@@ -10,6 +10,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SHARED_LIBRARIES := libcutils liblog libmedia
+LOCAL_EXPORT_SHARED_LIBRARY_HEADERS := libmedia
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
