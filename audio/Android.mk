9b686eb8164f46f803448701c4714a260ec53d7d
Eric Laurent <elaurent@google.com>
2016-11-28 17:47:43 -0800

    remove legacy audio policy
    Test: make.
    
    Change-Id: I8b04ba9e1ab3b3fc22cf1b1baba010664c47ecc6
    (cherry picked from commit 136bad9bf4319787241593480d9a63085d1e3e35)

--- audio/Android.mk
+++ audio/Android.mk
@@ -19,49 +19,4 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
 
 include $(BUILD_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    AudioPolicyManagerBase.cpp \
-    AudioPolicyCompatClient.cpp \
-    audio_policy_hal.cpp
-
-ifeq ($(AUDIO_POLICY_TEST),true)
-  LOCAL_CFLAGS += -DAUDIO_POLICY_TEST
-endif
-
-LOCAL_SHARED_LIBRARIES := libmedia
-LOCAL_STATIC_LIBRARIES := libmedia_helper
-LOCAL_MODULE := libaudiopolicy_legacy
-LOCAL_CFLAGS += -Wno-unused-parameter
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/../include
-
-include $(BUILD_STATIC_LIBRARY)
-
-# The default audio policy, for now still implemented on top of legacy
-# policy code
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := \
-    AudioPolicyManagerDefault.cpp
-
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libmedia \
-    libutils \
-    liblog
-
-LOCAL_STATIC_LIBRARIES := \
-    libmedia_helper
-
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-    libaudiopolicy_legacy
-
-LOCAL_MODULE := audio_policy.default
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_CFLAGS := -Wno-unused-parameter
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
-
-include $(BUILD_SHARED_LIBRARY)
 
