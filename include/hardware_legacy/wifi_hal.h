626449000b12bed6099a36cd051988c9a8f1f9df
Purushottam Kushwaha <pkushwah@codeaurora.org>
2018-06-07 23:21:36 +0530

    Add wifi_add_or_remove_virtual_intf() to the legacy wifi hal
    This is used to implement add/remove virtual interfaces for legacy
    wifi drivers at runtime.
    
    Change-Id: I5e95fcc350b2e000dab8843d91423235d4310f03
    CRs-Fixed: 2257197

--- include/hardware_legacy/wifi_hal.h
+++ include/hardware_legacy/wifi_hal.h
@@ -464,6 +464,9 @@ typedef struct {
                                          wifi_roaming_config *roaming_config);
     wifi_error (*wifi_set_radio_mode_change_handler)(wifi_request_id id, wifi_interface_handle
                         iface, wifi_radio_mode_change_handler eh);
+    wifi_error (*wifi_add_or_remove_virtual_intf)(wifi_interface_handle iface,
+                                                  const char* ifname, u32 iface_type,
+                                                  bool create);
     wifi_error (*wifi_set_qpower)(wifi_interface_handle iface, u8 powersave);
 } wifi_hal_fn;
 wifi_error init_wifi_vendor_hal_func_table(wifi_hal_fn *fn);
