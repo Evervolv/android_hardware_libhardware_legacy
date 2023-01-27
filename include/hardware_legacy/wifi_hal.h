55f43fb91ee8e0395a48105d0219c2bba1bf037a
Oscar Shu <xshu@google.com>
2023-01-27 21:54:14 +0000

    Legacy wifi feature flag for AFC channel allowance
    Bug: 242917176
    Test: compile
    Change-Id: Ida30239103d751eb4c7ce5bf96e8068a92a77247

--- include/hardware_legacy/wifi_hal.h
+++ include/hardware_legacy/wifi_hal.h
@@ -454,6 +454,7 @@ void wifi_get_error_info(wifi_error err, const char **msg); // return a pointer 
 #define WIFI_FEATURE_SET_LATENCY_MODE   (uint64_t)0x40000000  // Support Latency mode setting
 #define WIFI_FEATURE_P2P_RAND_MAC       (uint64_t)0x80000000  // Support P2P MAC randomization
 #define WIFI_FEATURE_INFRA_60G          (uint64_t)0x100000000 // Support for 60GHz Band
+#define WIFI_FEATURE_AFC_CHANNEL        (uint64_t)0x200000000 // Support for setting 6GHz AFC channel allowance
 // Add more features here
 
 #define IS_MASK_SET(mask, flags)        (((flags) & (mask)) == (mask))
